545c9c9bbb6ad60e610274477910ace234ab95a3
Martin <schmidtmartin697@gmail.com>
2023-06-10 00:20:50 +0200

    Update

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
 window.onload = function setChangelogTextAreaDefaultText(){
   changelogTextArea = document.getElementById("changelogTextArea");
-  changelogTextArea.value = "» Changelog\n*~* ";
+  changelogTextArea.value = "» Changelog\n- ";
 };
 
 function getAndCopyPluginChangelog(source) {
   switch (source) {
     case "discord":
       changelogText = prepareDiscordChangelog();
+      if (changelogText == null || changelogText == "") return;
       break;
     case "others":
       changelogText = prepareOthersChangelog();
@@ -21,9 +22,17 @@ function getAndCopyPluginChangelog(source) {
 function prepareDiscordChangelog() {
   var updateType = getUpdateType();
   var version = document.getElementById("pluginVersion").value;
-  changelogText = "Version: " + version + " Type: " + updateType;
-  alert(changelogText);
-  return changelogText;
+  var spigotChangelog = document.getElementById("spigotChangelog").value;
+  var modrinthChangelog =  document.getElementById("modrinthChangelog").value;
+
+  if (validateValues(version, spigotChangelog, modrinthChangelog) == false) return;
+  if (updateType == false) return;
+
+  title = `@everyone :loudspeaker:  **${version} ${updateType} for Kudos is now released!**`;
+  footerOne = `:paperclip:  Changelog on SpigotMC: ${spigotChangelog}`;
+  footerTwo = `:paperclip:  Changelog on Modrinth: ${modrinthChangelog}`;
+  finalChangelog = `${title} \n\n ${footerOne} \n ${footerTwo}`;
+  return finalChangelog;
 }
 
 function prepareOthersChangelog() {
@@ -31,42 +40,64 @@ function prepareOthersChangelog() {
   return changelogText;
 }
 
-
 function copyTextClipboard(copyText) {
-  if (copyText.value == null || copyText.value == "") {
+  if (copyText == null || copyText == "") {
     alert("Es wurde kein Text zum kopieren gefunden!");
     return;
   }
-
-  // Select the text
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
+  navigator.clipboard.writeText(copyText);
 
   // Alert the copied text
-  alert("Copied the text: " + copyText.value);
-  return copyText;
+  alert("Copied the text: " + copyText);
 }
 
-
 function getUpdateType() {
-  var updateType = document.getElementsByName("updateType").value;
+  var updateType = null;
+  try {
+    updateType = document.querySelector('input[name="updateType"]:checked').value;
+  }
+  catch {
+    alert("Bitte gebe die Art des Updates an!");
+    return false;
+  }
+  // TODO Fehlerbehandlung
+  // try {
+  //   updateType = document.querySelector('input[name="updateType"]:checked').value == null
+  // }
+  // catch {
+  //   alert("Bitte wähle die Art des Updates!");
+  //   return false;
+  // }
+
   if (updateType == "Major" || updateType == "Minor") return updateType += " Update";
   if (updateType == "Patch") return updateType;
   if (updateType == "PRE") return updateType + "-" + document.getElementById("preVersionNumberField").value;
   return "UNDEFINED";
 } 
 
-function foo() {
+function showAndHidePreVersionNumberField() {
   if (document.getElementById("updateTypePRE").checked == true) {
     element = document.getElementById("preVersionNumberField");
-    element.style.visibility = "";
+    element.style.visibility = "visible";
   }
 }
 
-/*
-  PLACEHOLDERS FOR GITHUB, SPIGOTMC AND MODRINTH
-  '*~*' == - 
-*/
+function validateValues(version, spigotChangelog, modrinthChangelog) {
+  if (version != null || version != "") {
+    return true;
+  }
+  else {
+    alert("Bitte gebe eine Version an!");
+  }
+  if (spigotChangelog != null || spigotChangelog != "") {
+    return true;
+  } else {
+    alert("Bitte gebe den SpigotMC Changelog an!");
+  }
+  if (modrinthChangelog != null || modrinthChangelog != "") {
+    return true;
+  } else {
+    alert("Bitte gebe den Modrinth Changelog an!");
+  }
+  return false;
+}
